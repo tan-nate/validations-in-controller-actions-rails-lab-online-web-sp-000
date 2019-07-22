a68f201acd526afa7e3f5ea58b393d0ae6da5cd2
Nate Tan <nktan93@gmail.com>
2019-07-22 18:10:43 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 50}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
